e95f8e30a40aad2e9dda5572edc35ee3ee4aedbd
Shashank Mishra <shashank.mishra@a5labs.co>
2026-02-20 13:28:04 +0530

    added more levels, minor ui adjustments

--- assets/scripts/configs/GameConfig.ts
+++ assets/scripts/configs/GameConfig.ts
@@ -7,10 +7,13 @@ export interface LevelConfig {
 export const LEVELS: LevelConfig[] = [
     { id: 1, rows: 2, cols: 4 },
     { id: 2, rows: 2, cols: 6},
-    { id: 3, rows: 3, cols: 4 },
-    { id: 4, rows: 4, cols: 4 },
-    { id: 5, rows: 4, cols: 5 },
-    { id: 6, rows: 5, cols: 6 },
+    { id: 3, rows: 2, cols: 5},
+    { id: 4, rows: 3, cols: 4 },
+    { id: 5, rows: 3, cols: 6},
+    { id: 6, rows: 4, cols: 5 },
+    { id: 7, rows: 4, cols: 6 },
+    { id: 8, rows: 5, cols: 6 },
+
 ];
 
 export const TURN_SCORE = -10;
